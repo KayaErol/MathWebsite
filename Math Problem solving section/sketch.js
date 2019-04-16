e33e1aa8bbebc7741c367057f3b0cbea197260ae
KayaErol <35324305+KayaErol@users.noreply.github.com>
2019-04-16 10:58:47 -0700

    Add files via upload

---
+++ Math Problem solving section/sketch.js
@@ -0,0 +1,23 @@
+let problems = [
+  {
+    question: 'What is 2 + 2?',
+    answers: [
+      {
+        answer: '4',
+        correct: true
+      },
+      {
+        answer: '8',
+        correct: false
+      }
+    ]
+  }
+];
+
+function setup() {
+  createCanvas(800, 800);
+
+}
+function draw() {
+  background(255);
+}
